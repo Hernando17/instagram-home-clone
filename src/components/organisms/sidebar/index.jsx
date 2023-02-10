bf6efc234e80c7cf04f5e53b2d5062a93fcf6f09
Hernando17 <ginkotsu17@gmail.com>
2023-02-10 15:05:27 +0700

    Tablet Responsive update

--- src/components/organisms/sidebar/index.jsx
+++ src/components/organisms/sidebar/index.jsx
@@ -1,45 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faSearch, faCompass, faVideo, faMessage, faInbox, faAdd, faHeart } from '@fortawesome/free-solid-svg-icons'
-
+import { faHouse, faSearch, faCompass, faVideo, faMessage, faInbox, faAdd, faHeart, faBars } from '@fortawesome/free-solid-svg-icons'
+import { useSelector } from 'react-redux'
+import { currentUser } from '../../../redux/features/userSlice'
 
 export function Sidebar() {
+    const user = useSelector(currentUser)
+
     return (
         <div className="sidebar">
             <div className="sidebar-container">
                 <img className="sidebar-logo-instagram" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png" />
-                <div className="sidebar-action">
-                    <FontAwesomeIcon icon={faAdd} size="xl" />
-                    <FontAwesomeIcon icon={faHeart} size="xl" />
+                <div>
+                    <img className="sidebar-logo-ig" src="https://cdn-icons-png.flaticon.com/512/87/87390.png" />
+                    <div className="sidebar-action">
+                        <FontAwesomeIcon icon={faAdd} size="xl" />
+                        <FontAwesomeIcon icon={faHeart} size="xl" />
+                    </div>
+                    <ul>
+                        <li>
+                            <FontAwesomeIcon icon={faHouse} size="xl" />
+                            <span>Home</span>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faSearch} size="xl" />
+                            <span>Search</span>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faCompass} size="xl" />
+                            <span>Explore</span>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faVideo} size="xl" />
+                            <span>Reels</span>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faMessage} size="xl" />
+                            <span>Messages</span>
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon={faInbox} size="xl" />
+                            <span>Notifications</span>
+                        </li>
+                        <li><img className="sidebar-profile" src={user.pp} /></li>
+                    </ul>
                 </div>
-                <ul>
-                    <li>
-                        <FontAwesomeIcon icon={faHouse} />
-                        <span>Home</span>
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faSearch} />
-                        <span>Search</span>
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faCompass} />
-                        <span>Explore</span>
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faVideo} />
-                        <span>Reels</span>
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faMessage} />
-                        <span>Messages</span>
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faInbox} />
-                        <span>Notifications</span>
-                    </li>
-                    <li><span>Create</span></li>
-                    <li><span>Profile</span></li>
-                    <li><span>More</span></li>
-                </ul>
+                <FontAwesomeIcon className="sidebar-more" icon={faBars} size="xl" />
             </div>
         </div>
     )
